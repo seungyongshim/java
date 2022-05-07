a2487aa6a686981c4b5d6e02cfc30662675320c8
SeungYongShim <syshim@me.com>
2022-05-08 01:02:44 +0900

    .

--- src/main/java/Prelude.java
+++ src/main/java/Prelude.java
@@ -1,6 +1,6 @@
 import java.util.function.Function;
 
-public class Main {
+public class Prelude {
     @org.jetbrains.annotations.NotNull
     @org.jetbrains.annotations.Contract(pure = true)
     public static Function<Integer, Function<Integer, Integer>> add() {
--- src/test/java/PreludeTest.java
+++ src/test/java/PreludeTest.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MainTest
+public class PreludeTest
 {
     @Test
     void addSpec() {
         // act
-        var ret = Main.add().apply(1).apply(2);
+        var ret = Prelude.add().apply(1).apply(2);
 
         // asserts
         Assertions.assertEquals(3, ret);
