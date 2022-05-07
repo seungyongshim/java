60bc3148dc06d80513c011cef6978551ab6973e5
SeungYongShim <syshim@me.com>
2022-05-07 21:54:37 +0900

    .

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -1,8 +1,12 @@
 package com.sample;
 
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    Function<Integer, Function<Integer, Integer>> Add = x -> y -> x + y;
 }
